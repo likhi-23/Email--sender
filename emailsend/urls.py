6fc482c26f21c5422f152f1e3ff328a85da37285
Likhith karri <99127911+likhi-23@users.noreply.github.com>
2022-07-17 18:39:37 +0530

    Update urls.py

--- emailsend/urls.py
+++ emailsend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from emailSend import views
+from emailsend import views
 urlpatterns = [
      path('',views.index, name='index'),
     path('mail',views.send,name='mail')
