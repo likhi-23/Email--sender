6320462e27868f1072392968638d8e9818a5b075
Likhith karri <99127911+likhi-23@users.noreply.github.com>
2022-07-18 17:38:05 +0530

    Update urls.py

--- emailsender/urls.py
+++ emailsender/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
-    path('',include('emailSend.urls'))
+    path('',include('emailsend.urls'))
 ]
